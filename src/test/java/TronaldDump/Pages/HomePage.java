8aca3be1f7daf2cf79f1e53e2b2cf78b3cb7a715
danrblake <daniel.blake1@hotmail.co.uk>
2018-11-22 15:07:56 +0000

    Initial project set up, POM model started

---
+++ src/test/java/TronaldDump/Pages/HomePage.java
@@ -0,0 +1,7 @@
+package TronaldDump.Pages;
+import org.openqa.selenium.WebDriver;
+public class HomePage {
+
+    private WebDriver driver;
+    private String homePageURL = "https://www.tronalddump.io/";
+}
---
+++ src/test/java/TronaldDump/TronaldSite.java
@@ -0,0 +1,10 @@
+package TronaldDump;
+import org.openqa.selenium.WebDriver;
+public class TronaldSite {
+
+        private WebDriver driver;
+
+    public TronaldSite(WebDriver driver){
+        this.driver = driver;
+    }
+}
