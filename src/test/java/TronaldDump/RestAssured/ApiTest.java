63d56f7f82e808f5ff17c6eff29e8c3102594073
danrblake <daniel.blake1@hotmail.co.uk>
2018-11-23 12:02:39 +0000

    Minor update on search

--- src/test/java/TronaldDump/RestAssured/ApiTest.java
+++ src/test/java/TronaldDump/RestAssured/ApiTest.java
@@ -44,7 +44,7 @@ public class ApiTest {
         String[] quotes = new String[5];
         for (int i = 0; i < searchArray.length; i++) {
             quotes[i] = site.firstSearchResult(searchArray[i]);
-            given().header("accept","application/hal+json").when().get(searchArray[i]).then().statusCode(200).body("_embedded.quotes[0]", equalTo(quotes[i]));
+            given().contentType("application/json").when().get(searchArray[i]).then().statusCode(200).body("$._embedded.quotes[0]", equalTo(quotes[i]));
         }
     }
 }
